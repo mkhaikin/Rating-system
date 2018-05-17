ed491f3ea0acd62fbfec4edd587cbeea54ec84aa
Michael Khaikin <michael.khaikin@gmail.com>
2018-05-17 00:53:14 -0400

    changed attr value of chosen circles
    from true to rated

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ $(".rating-circle").hover(function(){
 	$(this).removeClass("rating-chosen") + $(this).prevAll().removeClass("rating-chosen");
 		$(this).addClass("rating-hover") + $(this).prevAll().addClass("rating-hover");
 		$(".rating-circle").on("click",function(){//click event starts here
-			$("div[value='true']").removeAttr("value") + $("div[value='true']").removeClass("rating-chosen") + $(this).prevAll().removeClass("rating-chosen") + $(this).nextAll().removeClass("rating-chosen");
+			$("div[value='true']").removeAttr("value") + $("div[value='true']").removeClass("rating-chosen");
+      $(this).prevAll().removeClass("rating-chosen") + $(this).nextAll().removeClass("rating-chosen");
 			$(this).addClass("rating-chosen") + $(this).prevAll().css("rating-chosen");
 			$(this).attr("value", "true")+$(this).prevAll().attr("value", "true");
 	});
 		}, function(){
 			$(".rating-circle").removeClass("rating-hover");
 		if($(".rating-circle").attr("value")){
-			$("div[value='true']").addClass("rating-chosen");}
+			$("div[value='true']").addClass("rating-chosen");
+    }
 });
